3ef9dfba9524e5aa8124252df8bc811b4390f6e8
ArunPrasanth <arunprasanth.r@ptpll321.payoda.com>
2016-01-21 11:19:54 +0530

    Initial Commit

---
+++ ColorMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorMemory
+//
+//  Created by ArunPrasanth R on 21/01/16.
+//  Copyright © 2016 ArunPrasanth R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
